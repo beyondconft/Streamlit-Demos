5e1caae0398509803f66e3486203c4c0cfc665dd
Allan Alfonso <beyondconft@gmail.com>
2024-07-14 14:19:33 -0400

    Initial commit.

---
+++ Upload-Files-to-GCS/UploadFiles2GCS.py
@@ -0,0 +1,54 @@
+from google.cloud import storage
+import streamlit as st
+from io import StringIO
+import os
+from dotenv import load_dotenv, dotenv_values
+
+
+
+def upload_blob(bucket_name, destination_blob_name, file):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+    # The file to upload
+    # file = file-object
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    # Optional: set a generation-match precondition to avoid potential race conditions
+    # and data corruptions. The request to upload is aborted if the object's
+    # generation number does not match your precondition. For a destination
+    # object that does not yet exist, set the if_generation_match precondition to 0.
+    # If the destination object already exists in your bucket, set instead a
+    # generation-match precondition using its generation number.
+    generation_match_precondition = 0
+
+    #blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
+    blob.upload_from_file(file)
+
+    print(
+        f"File {destination_blob_name} uploaded to {bucket_name}."
+    )
+    
+    st.write("File:  ", destination_blob_name, "  uploaded.")
+
+
+#####
+
+
+st.title('Upload Files to Google Cloud Storage')
+
+# load variables from .env file
+load_dotenv()
+
+bucket_name = os.environ["BUCKET_NAME"]
+print(bucket_name)
+
+uploaded_files = st.file_uploader("Choose a file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    print(uploaded_file.name)
+    upload_blob(bucket_name, uploaded_file.name, uploaded_file)
